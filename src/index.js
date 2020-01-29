03cabb9406883f5ff8a62475f37f7f26bb49d5d2
raidencb <47615146+raidencb@users.noreply.github.com>
2020-01-30 06:54:43 +0800

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from './index.module.scss';
+
+ReactDOM.render(
+  <h1 className={styles.headings}>Hello World</h1>,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.module\.s(a|c)ss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]__[local]___[hash:base64:5]'
+              },
+              localsConvention: 'camelCase',
+              sourceMap: isDevelopment
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      },
+      {
+        test: /\.module\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]__[local]___[hash:base64:5]'
+              },
+              localsConvention: 'camelCase',
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        exclude: /\.module\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.scss']
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].[hash].bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: './src/index.html'
+    })
+  ],
+  devServer: {
+    contentBase: './dist'
+  }
+};
